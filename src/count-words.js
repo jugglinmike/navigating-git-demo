be2ad426af1e2ce501c59e42c694a93956074f82
Ben Alman <cowboy@rj3.net>
2014-09-01 14:43:59 -0400

    Implement basic "read from file" feature
    This feature depends on the basic implementation of the module, so it
    should not be merged until that work is complete.

--- src/count-words.js
+++ src/count-words.js
@@ -1,7 +1,26 @@
 'use strict';
+var readFile = require('fs').readFile;
 
 function countWords() {
 
 }
+
+/**
+ * Count the number of English words in a given file.
+ *
+ * @param {string} fileName - a path to the file whose words should be counted
+ * @param {function} callback - a function that will be invoked when the
+ *                              operation is complete
+ */
+countWords.fromFile = function(fileName, callback) {
+  readFile(fileName, { encoding: 'utf8' }, function(err, contents) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    callback(countWords(contents));
+  });
+};
 
 module.exports = countWords;
